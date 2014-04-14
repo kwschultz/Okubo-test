789ae8efe950312c5dd06e4be26772a3af8c3e43
Kasey W. Schultz <kwschultz@ucdavis.edu>
2014-04-14 14:43:52 -0700

    Latest edits

--- make_plot.py
+++ make_plot.py
@@ -6,15 +6,16 @@ reload(test_plot)
 import numpy as np
 import os
 import time
-
+from matplotlib import pyplot as plt
 
 sys.path.insert(0,"/home/kasey/PyVC/")
 from pyvc import vcplots
+from pyvc import vcanalysis
 from pyvc import *
 
 # Need to remove a cached file for Arial fonts to be used
 if os.path.isfile('/home/kasey/.matplotlib/fontList.cache'):
-    os.remove('/home/kasey/.matplotlib/fontList.ca30che')
+    os.remove('/home/kasey/.matplotlib/fontList.cache')
 #=============================================================================
 
 
@@ -63,57 +64,185 @@ for k in range(3):
 ##  ANIMATION SWITCHES & KNOBS
 #******************************
 #center_evnum = 91382    #pick a large event to be the centerpiece
-duration           = 100.0             # in years, make it small for test
-start_year         = 0.0
-EVENT_RANGE        = {'type':'year', 'filter':(start_year,start_year+duration)}
-#sim_file          = 'ALLCAL2_1-7-11_no-creep_dyn-05_st-20.h5'
-sim_file             = '../VCModels/ALLCAL2_1-7-11_no-creep/ALLCAL2_1-7-11_no-creep_dyn-0-5_st-5.h5'
+
+#sim_file             = '../VCModels/ALLCAL2_1-7-11_no-creep/ALLCAL2_1-7-11_no-creep_dyn-0-5_st-5.h5'
+#sim_file = 'ALLCAL2_3_4_14_southern_SAF_no-creep_dyn-0-5_st-20_10000yr.h5'
 #FIELD              = 'displacement'
 #LENGTH          = 10.0
 #FPS                   = 5.0
 #FADE                = 1.0
 #PLOT                = True
 #MIN_MAG_MARK   = 6.5
-output_directory      = 'animation_test_d/'
-text_out          = 'slip_time_series.txt'
 
-event_element_slips = {}
-slip_time_series = {}
+duration           = 100.0             # in years, make it small for test
+start_year         = 50.0
+DT                 = 0.1
+sim_time_range     = {'type':'year', 'filter':(start_year,start_year+duration)}
+SOUTH_SAF_SECS     = {'filter':(13,14,15,16,17,18)}
+sim_file           = 'ALLCAL2_1-7-11_no-creep_dyn-05_st-20.h5'
+#sim_file           = 'ALLCAL2_3_4_14_southern_SAF_no-creep_dyn-0-5_st-20_10000yr.h5'
+#output_directory   = 'time_series_test/'
+output_directory   = 'animation_south/'
+#evnum              = 55
+#out_file           = output_directory+'dg_field_'+str(evnum)+'.png'
+FIELD              = 'gravity'
+CUTOFF             = 1000.0
+LENGTH             = 50.0
+FPS                = 20.0
+MAG_FILTER         = "<= 7.5"
 
-#
+#vcanalysis.sim_info(sim_file,show=20,section_filter=SOUTH_SAF_SECS,magnitude_filter=MAG_FILTER)
+
+
+ALLCAL_EVIDS_in_SOCAL = [107556,183553,191433,76977,33928,252358,233381,225632,274396,77121,76324,138170,37557,83894,171713,253346,4699,170663,35068,51659]
+ 
+SOCAL_EVIDS = [2550,7094,13526,14367,13502,5471,15527,1731,963,1375,6022,259,8048,715,5343,12713,2300,9355,8750,13968]
+
+
+#event_range={'type':'year','filter':(2555,2655)}
+#output_directory = 'animation_test_v/'
+#vcplots.event_field_animation(sim_file, output_directory, event_range,
+#    field_type='gravity', fringes=True, padding=0.08, cutoff=None,
+#    animation_target_length=LENGTH/12.0, animation_fps = FPS, fade_seconds = 1.0,
+#    min_mag_marker = 6.5, force_plot=True)
+
+vcplots.event_field_evolution(sim_file, output_directory, sim_time_range,
+    field_type='gravity', fringes=True, padding=0.08, cutoff=CUTOFF,
+    animation_target_length=LENGTH, animation_fps = FPS,
+    min_mag_marker = 6.0, start_year = start_year, duration=duration,section_filter=SOUTH_SAF_SECS,
+    force_plot=True)
+
+
+
+
+"""
 with VCSimData() as sim_data:
     sim_data.open_file(sim_file)
+    geometry    = VCGeometry(sim_data)
+    events      = VCEvents(sim_data)
+    event_data = events.get_event_data(['event_magnitude', 'event_year', 'event_number'], event_range=sim_time_range)
     
-    geometry            = VCGeometry(sim_data)
-    events              = VCEvents(sim_data)
-    slip_rates          = geometry.get_slip_rates()
-    events_in_range     = events.get_event_data(['event_number', 'event_year'],event_range = EVENT_RANGE)
+    slip_rates  = geometry.get_slip_rates()
     
-    for evid in events_in_range['event_number']:
-        this_event_element_slips = events.get_event_element_slips(evid)
-        event_element_slips[evid] = this_event_element_slips
+    event_element_slips = {evid:events.get_event_element_slips(evid) for evid in event_data['event_number']}
+    
+    #avg_slip    = geometry.get_average_slip_time_series(event_data,event_element_slips,start_year=start_year,duration=duration,section_filter=SOUTH_SAF_SECS)
+    
+    #max_rate = []
+    #avg_rate = 0.0
+    #for bid in slip_rates.keys():
+    #    rate = slip_rates[bid]
+    #    avg_rate += rate
+    #    if max_rate == []:
+    #        max_rate = [bid,rate]
+    #    else:
+    #        if rate > max_rate[1]:
+    #            max_rate = [bid,rate]
                 
-sim_time   = 0.0
-DT         = 0.1 # 0.1yr evaluates field every 36.5 days
+    #avg_rate /= float(len(slip_rates.keys()))
+    
+    #print "max rate: ",max_rate[1]
+    #print "section:  ",geometry.get_section_name(geometry.sections_with_elements([max_rate[0]])[0])
+    #print "avg_rate: ",avg_rate
+    #print "max rate: ",max_rate[1]/avg_rate," avg_rate"
 
-for block_id in slip_rates.keys():
-    slip_time_series[block_id] = 0.0
-    
-time_values = np.arange(sim_time+DT,sim_time+duration+DT,DT)
+time_values = np.arange(start_year+DT,start_year+duration+DT,DT)
 
-for t in time_values:
-    events_this_period = []
-    for evid in events_in_range['event_number']:
-        if t-DT < events_in_range['event_year'][evid] <= t+DT:
-            events_this_period.append(evid)
-            
-        #loop over blocks and subtract slip_rate*dt to each
-        #for those involved in events, add their event slips
-        #append the total value to slip_time_series[block_id]
-            
+plt.clf()
+plt.plot(time_values,avg_slip)
+plt.title("Average time dependent slip on southern SAF")
+plt.grid(True)
+plt.xlabel("Simulated time")
+plt.ylabel("Average slip(t) [m]")
+plt.savefig("avg_slip.png",dpi=300)
+"""
+
+
+"""
+# event_element_slips = dictionary indexed by event_id with entries being dictionaries of slips indexed by block_id
+# slip_time_series    = dictionary indexed by block_id with entries being arrays of absolute slip at each time step
+
+#-------------------------------------------------------------------------------
+def get_slip_time_series(events,slip_rates,event_range,section_filter=None):
+    import pickle
     
+    # Convert slip rates from meters/second to meters/(decimal year)
+    CONVERSION = 3.15576*pow(10,7)
+    DT         = 0.1 # 0.1yr evaluates field every 36.5 days
     
+    with VCSimData() as sim_data:
+        sim_data.open_file(sim_file)
     
+        geometry            = VCGeometry(sim_data)
+        events              = VCEvents(sim_data)
+        slip_rates          = geometry.get_slip_rates(section_filter=section_filter)
+
+        if section_filter is not None:
+            events_in_range = events.get_event_data(['event_number', 'event_year','event_magnitude'],
+                                                      event_range = event_range,section_filter=section_filter)
+        else:
+            events_in_range = events.get_event_data(['event_number', 'event_year','event_magnitude'],
+                                                      event_range = event_range)
+        
+        event_element_slips = {evid:events.get_event_element_slips(evid) for evid in events_in_range['event_number']}
+
+    #Initialize blocks with 0.0 slip at time t=0.0
+    slip_time_series = {block_id:[0.0] for block_id in slip_rates.keys()}
+
+
+    #Initialize time steps to evaluate slip    
+    time_values = np.arange(start_year+DT,start_year+duration+DT,DT)
+
+    for k in range(len(time_values)):
+        if k>0:
+            # current time in simulation
+            right_now = time_values[k]
+        
+            # back slip all elements by subtracting the slip_rate*dt
+            for block_id in slip_time_series.keys():
+                last_slip = slip_time_series[block_id][k-1]
+                this_slip = slip_rates[block_id]*CONVERSION*DT
+                slip_time_series[block_id].append(last_slip-this_slip)
+
+            # check if any elements slip as part of simulated event in the window of simulation time
+            # between (current time - DT, current time), add event slips to the slip at current time 
+            # for elements involved
+            for evid in events_in_range['event_number']:
+                if right_now-DT < events_in_range['event_year'][evid] <= right_now:
+                    for block_id in event_element_slips[evid].keys():
+                        slip_time_series[block_id][k] += event_element_slips[evid][block_id]
+                               
+    out = open(outfile,'wb')                                         
+    pickle.dump(slip_time_series,out)
+    out.close()
+#------------------------------------------------------------------------------ 
+"""
+
+
+        
+"""
+for event in events_in_range['event_number']:
+    print "\nevent_id : %i\t year : %.5f\t magnitude : %.4f\t"%(event,events_in_range['event_year'][event],events_in_range['event_magnitude'][event])
+    print "involved elements: "+str(sorted(event_element_slips[event].keys()))
+"""
+
+
+"""
+    for block_id in slip_time_series.keys():
+        this_series = slip_time_series[block_id]
+        this_filename = output_directory+'element_'+str(block_id)+'_slips.png'
+
+        plt.clf()
+        plt.cla()
+        plt.plot(time_values,this_series,label="element "+str(block_id))
+        plt.axhline(y=0,xmin=0.0,xmax=time_values[-1],ls='--',color='k',lw=2)
+        plt.xlabel('simulated time [decimal years]')
+        plt.ylabel('accumulated slip on element [meters]')
+        plt.legend()
+        plt.savefig(this_filename,dpi=200)
+        print "written to: "+this_filename
+        
+"""
     
     #events         = VCEvents(sim_data)
     #center_evyear  = events.get_event_year(center_evnum)
@@ -162,17 +291,6 @@ vcplots.plot_event_field(sim_file, evnum, output_file=out_file, field_type=FIELD
                          fringes=FRINGES, padding=0.08, cutoff=None, save_file_prefix=None,
                          custom_lat_range=LAT_RANGE,custom_lon_range=LON_RANGE)
 #------------------------------------------------------------------------------
-"""
-
-
-"""
-sim_file = '../VCModels/ALLCAL2_1-7-11_no-creep/ALLCAL2_1-7-11_no-creep_dyn-0-5_st-5.h5'
-event_range={'type':'year','filter':(0,105)}
-output_directory = 'animation_test_v/'
-vcplots.event_field_animation(sim_file, output_directory, event_range,
-    field_type='potential', fringes=True, padding=0.08, cutoff=None,
-    animation_target_length=10.0, animation_fps = 2.0, fade_seconds = 1.0,
-    min_mag_marker = 5.5, force_plot=True)
 """
 
 
