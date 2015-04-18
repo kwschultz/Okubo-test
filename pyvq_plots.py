0d39324e759ee078eab7603520e7cb0e7a427fbc
kwschultz <kwschultz@ucdavis.edu>
2015-04-18 01:07:24 -0700

    Added many pyvq calls

--- pyvq_plots.py
+++ pyvq_plots.py
@@ -1,19 +1,192 @@
 #!/usr/bin/env python
 
-from subprocess import call
+from os import system
 
-pyvq      = "../vq/examples/pyvq.py"
-sim_tag   = "--event_file"
-sim_file  = "events_two_fault_100k.h5"
-file_type = "hdf5"
-file_tag  = "--event_file_type"
-plot_arg  = "--plot_freq_mag"
-
-arg_list = [pyvq,sim_tag, sim_file, file_tag, file_type, plot_arg]
-
-call(arg_list)
+TYPE = "fundamental_greens"
+# greens, fundamental_greens, single, Okubo, num_elements
 
 
+#  ---- Plotting Okubo Green's functions for single faults ----------------
+if TYPE == "greens":
+    plot       = "--greens"
+    field_type = ["geoid","geoid","gravity","dilat_gravity","gravity","dilat_gravity"]
+    plot_name  = ["strikeslip_dip90_LL_500x300km","thrust_dip45_500x300km",
+                  "strikeslip_dip90_LL_10x10km","strikeslip_dip90_LL_10x10km",
+                  "thrust_dip45_10x10km","thrust_dip45_10x10km"]
+    slip       = [10,10,5,5,5,5]
+    rake       = [0,90,0,0,90,90]
+    dip        = [90,45,90,90,45,45]
+    dttf       = [500,500,1000,1000,1000,1000]
+    cbar_max   = [0.01,0.05,50,15,500,30]
+    levels     = ["-.01 -.0075 -.005 -.0025 0 .0025 .005 .0075 .01",
+                  "-.05 -.04 -.03 -.02 -.01 0 .01 .02 .03 .04 .05",
+                  "-50 -40 -30 -20 -10 0 10 20 30 40 50",
+                  "-15 -10 -5 0 5 10 15", 
+                  "-500 -400 -300 -200 -100 0 100 200 300 400 500",
+                  "-30 -20 -10 0 10 20 30"]
+    Xmin       = [-500000, -500000, -5000, -5000, -5000, -5000]
+    Xmax       = [1000000, 1000000, 15000, 15000, 15000, 15000]
+    Ymin       = [-750000, -750000, -10000, -10000, -5000, -5000]
+    Ymax       = [750000, 750000, 10000, 10000, 15000, 15000]
+    L          = [500000, 500000, 10000, 10000, 10000, 10000]
+    W          = [300000, 300000, 10000, 10000, 10000, 10000]
+
+    for i in range(len(L)):
+        return_value = system("python ../vq/pyvq/pyvq.py {} --field_type {} --plot_name {} --uniform_slip {} --colorbar_max {} --levels {} --rake {} --dip {} --DTTF {} --L {} --W {} --Xmin {} --Xmax {} --Ymin {} --Ymax {}".format(plot, field_type[i], plot_name[i], slip[i], cbar_max[i], levels[i], rake[i], dip[i], dttf[i], L[i], W[i], Xmin[i], Xmax[i], Ymin[i], Ymax[i]))
+        
+        
+if TYPE == "fundamental_greens":
+    plot       = "--greens"
+    plot_name  = ["strikeslip_dip90_LL_10x10km","thrust_dip30_10x10km","normal_dip60_10x10km"]
+    field_type = "displacement"
+    slip       = [5,5,5]
+    rake       = [0,90,-90]
+    dip        = [90,30,60]
+    dttf       = 1000
+    if field_type == "gravity":
+        cbar_max   = [50,500,500]
+        levels     = ["-50 -40 -30 -20 -10 0 10 20 30 40 50",
+                      "-500 -400 -300 -200 -100 0 100 200 300 400 500",
+                      "-500 -400 -300 -200 -100 0 100 200 300 400 500"]
+    elif field_type == "displacement":
+        cbar_max   = [.2,2,2]
+        levels     = ["-.2 -.15 -.1 -.05 -.01 0 .01 .05 .1 .15 .2",
+                      "-2 -1.5 -1 -.5 -.1 0 .1 .5 1 1.5 2",
+                      "-2 -1.5 -1 -.5 -.1 0 .1 .5 1 1.5 2"]
+    elif field_type == "dilat_gravity":
+        cbar_max   = [20,20,20]
+        levels     = ["-20 -15 -10 -5 -2 0 2 5 10 15 20",
+                      "-20 -15 -10 -5 -2 0 2 5 10 15 20",
+                      "-20 -15 -10 -5 -2 0 2 5 10 15 20"]
+    L          = 10000
+    W          = 10000
+    Nx         = 2000
+    Ny         = 2000
+    # Okubo range
+    #Xmin       = [-5000, -5000, -5000]
+    #Xmax       = [15000, 15000, 15000]
+    #Ymin       = [-10000, -5000, -5000]
+    #Ymax       = [10000, 15000, 15000]
+    # Better looking range
+    Xmin       = [-10000, -10000, -10000]
+    Xmax       = [20000, 20000, 20000]
+    Ymin       = [-15000, -10000, -10000]
+    Ymax       = [15000, 20000, 20000]
+
+    for i in range(len(plot_name)):
+        return_value = system("python ../vq/pyvq/pyvq.py {} --field_type {} --plot_name {} --uniform_slip {} --colorbar_max {} --levels {} --rake {} --dip {} --DTTF {} --L {} --W {} --Xmin {} --Xmax {} --Ymin {} --Ymax {} --Nx {} --Ny {}".format(plot, field_type, plot_name[i], slip[i], cbar_max[i], levels[i], rake[i], dip[i], dttf, L, W, Xmin[i], Xmax[i], Ymin[i], Ymax[i], Nx, Ny))
+# --------------------------------------------------------------------------
+
+
+
+
+
+# ---- Plotting the fields of faults with prescribed slip
+if TYPE == "single":
+    plot    = "--field_plot"
+    fields  = ["gravity", "dilat_gravity"]
+    #fields = ["gravity"]
+    slip    = 5
+    #models = ["single_normal_dip60_switch_fault_10000.txt","single_normal_dip60_fault_10000.txt","single_thrust_dip30_fault_10000.txt","single_vert_strikeslip_RightLateral_fault_10000.txt","single_normal_dip45_switch_fault_10000.txt","single_thrust_dip45_switch_fault_10000.txt"]
+    models  = ["single_vert_strikeslip_LeftLateral_fault_10000.txt","single_normal_dip45_fault_10000.txt","single_thrust_dip45_fault_10000.txt"]
+    for model in models:
+        print(model)
+        for field in fields:
+            if field == "gravity":
+                if "strikeslip" in model.split("_"):
+                    cbar_max = 50
+                    levels = "-50 -40 -30 -20 -10 0 10 20 30 40 50"
+                else:
+                    cbar_max = 500
+                    levels = "-500 -400 -300 -200 -100 0 100 200 300 400 500"
+            elif field == "dilat_gravity":
+                if "strikeslip" in model.split("_"):
+                    cbar_max = 15
+                    levels = "-15 -10 -5 0 5 10 15"
+                else:
+                    cbar_max = 30
+                    levels = "-30 -20 -10 0 10 20 30"
+                    
+            return_value = system("python ../vq/pyvq/pyvq.py {} --model_file {} --field_type {} --uniform_slip {} --colorbar_max {} --levels {} --small_model".format(plot, model, field, slip, cbar_max, levels))
+# --------------------------------------------------------------------------
+
+
+
+
+
+# ---- Plotting the fields of faults with prescribed slip
+if TYPE == "Okubo":
+    plot    = "--field_plot"
+    fields  = ["geoid","gravity", "dilat_gravity"]
+    models  = ["single_vert_strikeslip_LeftLateral_fault_10000.txt","single_thrust_dip45_fault_10000.txt","thrust_dip45_10x10km_fault_2000mElements.txt","thrust_dip45_500x300km_fault_100kmElements.txt","vert_strikeslip_LeftLateral_500x300km_fault_100kmElements.txt","single_thrust_dip45_switch_10x10km_fault_10000.txt","thrust_dip45_switch_10x10km_fault_2000mElements.txt"]
+    for model in models:
+        print(model)
+        for field in fields:
+            if field == "gravity":
+                slip = 5
+                extra = "--small_model"
+                if "strikeslip" in model.split("_"):
+                    cbar_max = 50
+                    levels = "-50 -40 -30 -20 -10 0 10 20 30 40 50"
+                else:
+                    cbar_max = 500
+                    levels = "-500 -400 -300 -200 -100 0 100 200 300 400 500"
+            elif field == "dilat_gravity":
+                slip = 5
+                extra = "--small_model"
+                if "strikeslip" in model.split("_"):
+                    cbar_max = 15
+                    levels = "-15 -10 -5 0 5 10 15"
+                else:
+                    cbar_max = 30
+                    levels = "-30 -20 -10 0 10 20 30"
+                
+            
+            elif field == "geoid":
+                slip = 10
+                extra = ""
+                if "strikeslip" in model.split("_"):
+                    cbar_max = 1
+                    levels = "-1 -.75 -.5 -.25 0 .25 .5 .75 1"
+                else:
+                    cbar_max = 5
+                    levels = "-5 -4 -3 -2 -1 0 1 2 3 4 5"
+                
+            if field == "geoid" and not "500x300km" in model.split("_"):
+                print "skipped"
+            else:
+                return_value = system("python ../vq/pyvq/pyvq.py {} --model_file {} --field_type {} --uniform_slip {} --colorbar_max {} --levels {} {}".format(plot, model, field, slip, cbar_max, levels, extra))
+# --------------------------------------------------------------------------
+
+
+
+# ------ Seeing how the field changes when increasing the number of elements
+if TYPE == "num_elements":
+    plot    = "--field_plot"
+    field   = "gravity"
+    model_dir = "../VQModels/"
+    models  = ["single_thrust_dip45_switch_10x10km_fault_10000.txt","thrust_dip45_switch_10x10km_fault_5000mElements.txt","thrust_dip45_switch_10x10km_fault_3333mElements.txt","thrust_dip45_switch_10x10km_fault_2500mElements.txt","thrust_dip45_switch_10x10km_fault_2000mElements.txt","thrust_dip45_switch_10x10km_fault_1kmElements.txt"]
+    for model in models:
+        model = model_dir+model
+        print(model)
+        if field == "gravity":
+            slip = 5
+            if "strikeslip" in model.split("_"):
+                cbar_max = 50
+                levels = "-50 -40 -30 -20 -10 0 10 20 30 40 50"
+            else:
+                cbar_max = 500
+                levels = "-500 -400 -300 -200 -100 0 100 200 300 400 500"
+        elif field == "dilat_gravity":
+            slip = 5
+            if "strikeslip" in model.split("_"):
+                cbar_max = 15
+                levels = "-15 -10 -5 0 5 10 15"
+            else:
+                cbar_max = 30
+                levels = "-30 -20 -10 0 10 20 30"
+
+        return_value = system("python ../vq/pyvq/pyvq.py {} --model_file {} --field_type {} --uniform_slip {} --colorbar_max {} --levels {} --small_model".format(plot, model, field, slip, cbar_max, levels))
 
 
 
--- pyvq_plots.sh
+++ pyvq_plots.sh
@@ -1,17 +1,106 @@
 #!/bin/bash
 
+# Ignore this PYTHONPATH
+#PYTHONPATH=/Users/kasey/vq/build/quakelib/python/
 
-python ../vq/examples/pyvq.py --event_file events_three_fault_1kyr_dyn0-5_BASSgen1.h5 --event_file_type 'hdf5' --plot_prob_vs_t_fixed_dt
+#python ../vq/pyvq/pyvq.py --event_file events_faultIDfix_all_cal_fault_5km_10kyr_dyn0-5_BASSgen0.h5 --event_file_type 'hdf5' --all_stat_plots
 
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/single_vert_strikeslip_LeftLateral_fault_10000.txt --field_plot --field_type "dilat_gravity" --uniform_slip 5 --colorbar_max 15 --levels -15 -10 -5 0 5 10 15 
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/single_vert_strikeslip_RightLateral_fault_10000.txt --field_plot --field_type "dilat_gravity" --uniform_slip 5 --colorbar_max 15 --levels -15 -10 -5 0 5 10 15 
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/thrust_dip45_switch_10x10km_fault_3333mElements.txt --field_plot --field_type "dilat_gravity" --uniform_slip 5 --colorbar_max 30 --levels -30 -20 -10 0 10 20 30 --small_model
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/vert_strikeslip_LeftLateral_strike270_fault_10km.txt --field_plot --field_type "gravity" --uniform_slip 5 --colorbar_max 50 --levels -50 -40 -30 -20 -10 0 10 20 30 40 50 --small_model
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_all_cal_fault_5km_20kyr_noShocks.h5 --event_file_type 'hdf5' --field_plot --field_type "gravity" --colorbar_max 20 --levels -20 -10 -5 -2.5 -1 0 1 2.5 5 10 20 --event_id 440
+
+#---------------------------------------------
+python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_allcal_5km_250kyr.h5 --event_file_type 'hdf5' --field_plot --field_type "gravity" --colorbar_max 10 --event_id 128 --levels -10 -5 -2.5 -1.25 0 1.25 2.5 5 10
+
+python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_allcal_5km_250kyr.h5 --event_file_type 'hdf5' --field_plot --field_type "dilat_gravity" --colorbar_max 10 --event_id 128 --levels -10 -5 -2.5 -1.25 0 1.25 2.5 5 10
+
+##python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_allcal_5km_250kyr.h5 --event_file_type 'hdf5' --field_plot --field_type "geoid" --colorbar_max .01 --event_id 128 --levels -.01 -.005 -.0025 -.00125 0 .00125 .0025 .005 .01 
+
+python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_allcal_5km_250kyr.h5 --event_file_type 'hdf5' --field_plot --field_type "insar" --event_id 128
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_allcal_5km_250kyr.h5 --event_file_type 'hdf5' --all_stat_plots --wc94 --min_magnitude 4
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_norcal_fault_5km_5kyr_dyn0-5_BASSgen0.h5 --event_file_type 'hdf5' --all_stat_plots --wc94
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --traces
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_norcal_fault_5km_5kyr_dyn0-5_BASSgen0.h5 --event_file_type 'hdf5' --field_plot --field_type "gravity" --event_id 1414 --colorbar_max 20
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --event_file events_allcal_5km_250kyr.h5 --event_file_type 'hdf5' --plot_cond_prob_vs_t --plot_waiting_times --min_magnitude 7.5 --use_sections 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140
+
+#--use_sections 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_3000.h5 --traces --use_sections 126 127 128 129 130 131 132 133 134 135 136 137 138 139 140
+
+#---------------------------------------------
+
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/thrust_dip45_switch_10x10km_fault_3333mElements.txt --field_plot --field_type "gravity" --uniform_slip 5 --colorbar_max 500 --levels -500 -400 -300 -200 -100 0 100 200 300 400 500 --small_model
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/allcal_fault_5000.h5 --traces
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/thrust_dip45_switch_500x300km_fault_50000mElements.txt --field_plot --field_type "geoid" --uniform_slip 10 --colorbar_max .05 --levels -.05 -.04 -.03 -.02 -.01 0 .01 .02 .03 .04 .05 --small_model
+
+#python ../vq/pyvq/pyvq.py --model_file ../VQModels/vert_strikeslip_LeftLateral_500x300km_fault_100kmElements.txt --field_plot --field_type "geoid" --uniform_slip 10 --colorbar_max .01 --levels -.01 -.0075 -.005 -.0025 0 .0025 .005 .0075 .01 --small_model
+
+#python ../vq/pyvq/pyvq.py --greens --field_type "gravity" --plot_name "strikeslip_dip90_LL" --uniform_slip 5 --colorbar_max 50 --levels -50 -40 -30 -20 -10 0 10 20 30 40 50 --rake 0 --dip 90 --DTTF 1000
+
+#python ../vq/pyvq/pyvq.py --greens --field_type dilat_gravity --plot_name strikeslip_dip90_LL --uniform_slip 5 --colorbar_max 15 --levels -15 -10 -5 0 5 10 15 --rake 0 --dip 90 --DTTF 1000
+
+#python ../vq/pyvq/pyvq.py --event_file events_eqsim_ucerf2_no-creep_no_taper_noAftershocks_8kyr_0-5dyn_3km.h5 --event_file_type 'hdf5' --plot_freq_mag 4
+
+#python ../vq/pyvq/pyvq.py --event_file events_small_ca_6fault_50kyr_dyn0-5_BASSgen0.h5 --event_file_type 'hdf5' --plot_freq_mag 4
+
+
+#thrust_dip30_fault_10km.txt
+#vert_strikeslip_fault_10km.txt
+#normal_dip60_fault_10km.txt
+#events_small_ca_6fault_50kyr_dyn0-5_BASSgen0.h5
 #events_two_fault_50k_multiproc_noAftershocks.h5
 #ALLCAL2_1-7-11_no-creep_dyn-05_st-20.h5
-#events_two_fault_1k_multiproc_noAftershocks.h5_argument('--plot_freq_mag', required=False, action='store_true',
+#events_two_fault_1k_multiproc_noAftershocks.h5
 #events_three_fault_1kyr_dyn0-5_BASSgen1.h5
 
-# Plot options
-# --plot_freq_mag
+# ----------------- Example usage --------------------------
+
+# Frequency magnitude plot for all events in the sim file
+# python path/to/vq/examples/pyvq.py --event_file path/to/sim_file.h5 --event_file_type 'hdf5' --plot_freq_mag 1
+
+# Frequency magnitude plot including a b=1 line, only using EQs that produce slip on sections 1,2,3 (must also specify a model file)
+# python path/to/vq/examples/pyvq.py --event_file path/to/sim_file.h5 --event_file_type 'hdf5' --plot_freq_mag 1 --model_file path/to/model.txt --use_sections 1 2 3
+
+# Magnitude vs Rupture Area plot for all events in the sim file
+# python path/to/vq/examples/pyvq.py --event_file path/to/sim_file.h5 --event_file_type 'hdf5' --plot_mag_rupt_area
+
+# Plot prob vs time of 3.0 < Mag < 6.0 earthquakes for all events in the sim file
+# python path/to/vq/examples/pyvq.py --event_file path/to/sim_file.h5 --event_file_type 'hdf5' --plot_prob_vs_t --min_magnitude 3.0 --max_magnitude 6.0
+
+# Plot conditional prob vs time of Mag > 5.5 earthquakes, and plotting a weibull curve with beta=1.2 and tau=19
+# python path/to/vq/examples/pyvq.py --event_file path/to/sim_file.h5 --event_file_type 'hdf5' --plot_cond_prob_vs_t --min_magnitude 5.5 --beta 1.2 --tau 19
+
+# Plot waiting times vs time for Mag > 6.0 earthquakes
+# python path/to/vq/examples/pyvq.py --event_file path/to/sim_file.h5 --event_file_type 'hdf5' --plot_waiting_times --min_magnitude 6.0
+
+
+# ----- Currently working plot options (with examples) -------------------------------------------
+# --plot_freq_mag 1 (normal) or 2 (add a b=1 line) or 3 (add UCERF2 observed rates with errorbars) or 4 (add b=1 and UCERF2 rates)
 # --plot_mag_rupt_area
 # --plot_mag_mean_slip
 # --plot_prob_vs_t
 # --plot_prob_vs_t_fixed_dt
+# --plot_cond_prob_vs_t
+# --plot_waiting_times
+# --min_magnitude 5.0
+# --max_magnitude 7.9
+# --use_sections 1 2 3 4
+# --field_plot
+# --field_type
+# --field_savefile
+# --colorbar_max 20
 
